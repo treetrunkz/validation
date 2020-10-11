b73a133153df071609c1e70f87366c606d5bd63f
treetrunkz <44754821+treetrunkz@users.noreply.github.com>
2020-10-11 08:12:26 -0700

    Update script.js

--- jqueryvalidate/script.js
+++ jqueryvalidate/script.js
@@ -1,19 +1,11 @@
-// Wait for the DOM to be ready
+
 $(function() {
-    // Initialize form validation on the registration form.
-    // It has the name attribute "registration"
     $("form[name='registration']").validate({
-        // Specify validation rules
         rules: {
-            // The key name on the left side is the name attribute
-            // of an input field. Validation rules are defined
-            // on the right side
             firstname: "required",
             lastname: "required",
             email: {
                 required: true,
-                // Specify that email should be validated
-                // by the built-in "email" rule
                 email: true
             },
             password: {
@@ -25,7 +17,6 @@ $(function() {
                 equalTo: "#password"
             }
         },
-        // Specify validation error messages
         messages: {
             firstname: "Please enter your firstname",
             lastname: "Please enter your lastname",
@@ -39,8 +30,6 @@ $(function() {
             },
             email: "Please enter a valid email address"
         },
-        // Make sure the form is submitted to the destination defined
-        // in the "action" attribute of the form when valid
         submitHandler: function(form) {
             form.submit();
         }
